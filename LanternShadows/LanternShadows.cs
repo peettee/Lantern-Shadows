e43895977c70ba20fef5924d94f1e9555124024b
Pierre Dupont <pierre.dupont@revvis.com>
2021-01-09 05:57:46 +0100

    feat: use settings to determine shadow type

--- LanternShadows/LanternShadows.cs
+++ LanternShadows/LanternShadows.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-namespace pp.RaftMods.EnhancedTorchLight
+namespace pp.RaftMods.LanternShadows
 {
     /// <summary>
     /// Enables realtime shadows on lantern buildables and other block lights.
     /// </summary>
-    public class CEnhancedTorchlight : Mod
+    public class CLanternShadows : Mod
     {
-        public const string VERSION = "0.0.1";
-        public const string APP_IDENT = "pp.RaftMods.EnhancedTorchLight";
+        public const string VERSION = "1.0.0";
+        public const string APP_IDENT = "pp.RaftMods.LanternShadows";
 
         public static bool PreventDayNightLightSwitch { get; set; } = false;
-        public static LightShadows ShadowType { get; set; } = LightShadows.Soft;
+
+        private static LightShadows LightShadowType => mi_settings.Current.graphics.shadowType == ShadowQuality.All ? LightShadows.Soft :
+                                                        mi_settings.Current.graphics.shadowType == ShadowQuality.HardOnly ? LightShadows.Hard :
+                                                            LightShadows.None;
 
         private static List<Light> mi_sceneLights;
+        private static Settings mi_settings;
+
         private Harmony mi_harmony;
 
         private void Start()
@@ -25,45 +31,66 @@ namespace pp.RaftMods.EnhancedTorchLight
             mi_harmony = new Harmony(APP_IDENT);
             mi_harmony.PatchAll(Assembly.GetExecutingAssembly());
 
-            if (RAPI.IsCurrentSceneGame())
-            {
-                mi_sceneLights = GameObject.FindObjectsOfType<LightSingularity>()
-                                    .Select(_o => _o.LightComponent)
-                                    .ToList();
-                foreach(var light in mi_sceneLights)
-                {
-                    light.shadows = ShadowType;
-                }
-            }
+            mi_settings = ComponentManager<Settings>.Value;
 
-            Debug.Log("EnhancedTorchLight v. " + VERSION + " loaded.");
+            LoadLights();
+
+            SceneManager.activeSceneChanged -= OnSceneLoaded;
+            SceneManager.activeSceneChanged += OnSceneLoaded;
+
+            Debug.Log("LanternShadows v. " + VERSION + " loaded.");
         }
 
         private void OnDestroy()
         {
-            if(mi_sceneLights != null)
+            if (mi_sceneLights != null)
             {
-                foreach(var light in mi_sceneLights)
+                foreach (var light in mi_sceneLights)
                 {
                     if (light) light.shadows = LightShadows.None;
                 }
                 mi_sceneLights = null;
             }
 
-            if(mi_harmony != null)
+            if (mi_harmony != null)
             {
                 mi_harmony.UnpatchAll(APP_IDENT);
                 mi_harmony = null;
+            }
+
+            mi_settings = null;
+
+            SceneManager.activeSceneChanged -= OnSceneLoaded;
+        }
+
+        private static void LoadLights()
+        {
+            if (RAPI.IsCurrentSceneGame())
+            {
+                mi_sceneLights = GameObject.FindObjectsOfType<LightSingularity>()
+                                    .Select(_o => _o.LightComponent)
+                                    .ToList();
+
+                var shadowType = LightShadowType;
+                foreach (var light in mi_sceneLights)
+                {
+                    light.shadows = shadowType;
+                }
             }
         }
 
         private static void RegisterLight(Light _light)
         {
-            _light.shadows = ShadowType;
+            _light.shadows = LightShadowType;
             if (!mi_sceneLights.Contains(_light))
             {
                 mi_sceneLights.Add(_light);
             }
+        }
+
+        private void OnSceneLoaded(Scene _oldScene, Scene _newScene)
+        {
+            LoadLights();
         }
 
         #region PATCHES
@@ -76,7 +103,7 @@ namespace pp.RaftMods.EnhancedTorchLight
             {
                 if (__result == null) return;
 
-                if(mi_sceneLights == null)
+                if (mi_sceneLights == null)
                 {
                     mi_sceneLights = new List<Light>();
                 }
@@ -91,11 +118,7 @@ namespace pp.RaftMods.EnhancedTorchLight
 
                 //if there is no raft light controller available try to search for other lights below the block to search for mod item lights
                 var lights = __result.GetComponentsInChildren<Light>(true);
-                if (lights.Length == 0)
-                {
-                    Debug.LogWarning("No light component(s) on block " + __result.name);
-                    return;
-                }
+                if (lights.Length == 0) return;
 
                 foreach (var light in lights)
                 {
@@ -114,7 +137,7 @@ namespace pp.RaftMods.EnhancedTorchLight
 
         [HarmonyPatch(typeof(LightSingularityExternal), "UpdateLightConnectivity")]
         public class HarmonyPatch_LightSingularityExternal_UpdateLightConnectivity
-        { 
+        {
             //do not morph light sources into singularity
             //Update light connectivity merges lights, need to prevent the method from executing.
             [HarmonyPrefix]
@@ -150,6 +173,16 @@ namespace pp.RaftMods.EnhancedTorchLight
             //Do not switch off torch lights during daytime
             [HarmonyPrefix]
             private static bool NightLightController_Update() => !PreventDayNightLightSwitch;
+        }
+
+        [HarmonyPatch(typeof(Settings), "SaveAll")]
+        public class HarmonyPatch_Settings_SaveAll
+        {
+            [HarmonyPostfix]
+            private static void Settings_SaveAll()
+            {
+                LoadLights();   
+            }
         }
         #endregion
     }
